927457842b449f73917969a4736dd81800225935
Vadim01j12feradim <izmael.guzman7281@alumnos.udg.mx>
2025-01-16 23:58:36 -0600

    asdsad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { IndexComponent } from './index/index.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,IndexComponent],
   templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  // template: `<h1>Hi izmael</h1>`,
+  // styles:['*{background-color:red}']
+  // styleUrl: './app.component.css'
   styleUrl: './app.component.css'
 })
+
+
+
 export class AppComponent {
   title = 'SPA';
 }
+
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { IndexComponent } from './app/index/index.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  // bootstrapApplication(IndexComponent, appConfig)
+  // .catch((err) => console.error(err));
